87d54c461e42e018ef2737dfd6fefc8db13a89a4
WickyChandra <WickyChandra@DESKTOP-846NUOS>
2022-05-23 14:01:04 +0700

    edit nilai

--- JavaFundamental9/src/com/project/day2/Konversi.java
+++ JavaFundamental9/src/com/project/day2/Konversi.java
@@ -4,7 +4,7 @@ public class Konversi {
 
 	public static void main(String[] args) {
 		
-		int numb1 = 700;
+		int numb1 = 789;
 		String numb2 = "200";
 		
 		double result = numb1 * Double.valueOf(numb2);
